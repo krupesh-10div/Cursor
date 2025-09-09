98600638df907edc634fcef06b96ed15fb94bf4a
KrupeshG <166097429+KrupeshG@users.noreply.github.com>
2025-09-09 17:22:27 +0530

    Add files via upload

---
+++ wp-content/plugins/icart-dynamic-landing/templates/product-sections/steller.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('ABSPATH')) { exit; }
+?>
+<section class="icart-dl__product-section icart-dl__product-section--icart">
+	<h2 class="icart-dl__section-title">Steller</h2>
+	<ul class="icart-dl__bullets">
+		<li>Steller</li>
+		<li>Steller-ons</li>
+		<li>Steller</li>
+	</ul>
+	<p>
+		<a class="button" href="https://apps.shopify.com/Steller">Try Steller free</a>
+	</p>
+</section>
