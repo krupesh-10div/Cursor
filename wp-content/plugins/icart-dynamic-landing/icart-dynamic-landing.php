a0ba431e303443c9496a0eb30a9a82ef789f2963
KrupeshG <166097429+KrupeshG@users.noreply.github.com>
2025-09-10 08:37:49 +0530

    Update icart-dynamic-landing.php

--- wp-content/plugins/icart-dynamic-landing/icart-dynamic-landing.php
+++ wp-content/plugins/icart-dynamic-landing/icart-dynamic-landing.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: iCart Dynamic Landing
+Plugin Name: Dynamic Landing Page
 Description: Dynamic landing page that adapts to user search keywords with Perplexity-generated content and CSV-based product recommendations.
 Version: 1.0.0
 Author: Identixweb
@@ -10,29 +10,29 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-define('ICART_DL_VERSION', '1.0.0');
-define('ICART_DL_PLUGIN_FILE', __FILE__);
-define('ICART_DL_PLUGIN_DIR', plugin_dir_path(__FILE__));
-define('ICART_DL_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('DL_VERSION', '1.0.0');
+define('DL_PLUGIN_FILE', __FILE__);
+define('DL_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('DL_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 // Autoload classes (simple loader)
 spl_autoload_register(function ($class) {
 	if (strpos($class, 'ICartDL_') === 0) {
 		$short = str_replace('ICartDL_', '', $class);
 		$short = str_replace('_', '-', $short);
-		$file = ICART_DL_PLUGIN_DIR . 'inc/' . 'class-' . strtolower($short) . '.php';
+		$file = DL_PLUGIN_DIR . 'inc/' . 'class-' . strtolower($short) . '.php';
 		if (file_exists($file)) {
 			require_once $file;
 		}
 	}
 });
 
-require_once ICART_DL_PLUGIN_DIR . 'inc/helpers.php';
+require_once DL_PLUGIN_DIR . 'inc/helpers.php';
 
 function icart_dl_init() {
 	// Ensure transients are namespaced
-	if (!defined('ICART_DL_TRANSIENT_PREFIX')) {
-		define('ICART_DL_TRANSIENT_PREFIX', 'icart_dl_');
+	if (!defined('DL_TRANSIENT_PREFIX')) {
+		define('DL_TRANSIENT_PREFIX', 'icart_dl_');
 	}
 
 	// Shortcode no longer required (direct routing)
@@ -43,7 +43,7 @@ function icart_dl_init() {
 	}
 
 	// Sync landing map from sample keywords folder
-	icart_dl_sync_landing_map_from_samples();
+	dl_sync_landing_map_from_samples();
 
 	// Register rewrite rules and query vars
 	add_filter('query_vars', function($vars){
@@ -66,7 +66,7 @@ function icart_dl_init() {
 	// Route to plugin template when icart_slug is present
 	add_filter('template_include', function($template){
 		if (get_query_var('icart_slug')) {
-			$tpl = ICART_DL_PLUGIN_DIR . 'templates/landing.php';
+			$tpl = DL_PLUGIN_DIR . 'templates/landing.php';
 			if (file_exists($tpl)) {
 				return $tpl;
 			}
@@ -76,16 +76,16 @@ function icart_dl_init() {
 
 	// Flush rewrite rules if requested by settings update
 	add_action('init', function(){
-		if (get_transient('icart_dl_flush_rewrite')) {
+		if (get_transient('dl_flush_rewrite')) {
 			flush_rewrite_rules();
-			delete_transient('icart_dl_flush_rewrite');
+			delete_transient('dl_flush_rewrite');
 		}
 	}, 99);
 }
 add_action('plugins_loaded', 'icart_dl_init');
 
 function icart_dl_enqueue_assets() {
-	wp_register_style('icart-dl-style', ICART_DL_PLUGIN_URL . 'assets/css/style.css', array(), ICART_DL_VERSION);
+	wp_register_style('icart-dl-style', DL_PLUGIN_URL . 'assets/css/style.css', array(), DL_VERSION);
 	wp_enqueue_style('icart-dl-style');
 }
 add_action('wp_enqueue_scripts', 'icart_dl_enqueue_assets');
