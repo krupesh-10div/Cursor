d0a96c89c783231ae42f70d9bebac117f0797305
KrupeshG <166097429+KrupeshG@users.noreply.github.com>
2025-09-16 08:33:42 +0530

    Delete build directory

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd90406170b956584fd37');
